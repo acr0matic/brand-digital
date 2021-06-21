a38596f5f702505ac2684d666a7bafd077e7509e
Nikita <main.acr0matic@gmail.com>
2021-06-22 01:47:27 +0300

    feat: главный экран "блога"

---
+++ src/blog.php
@@ -0,0 +1,20 @@
+<?php include_once('partials/header.php') ?>
+
+<!-- Основной контент -->
+<main>
+  <section id="blog-hero">
+    <div class="container">
+      <div class="blog-hero">
+        <h2 class="blog-hero__title page__title section__title">Автомобильная плёнка</h2>
+        <p class="blog-hero__text page__text section__text">«Когда вы вносите в мир правильную идею, люди не могут ее не заметить». (Сет Годин)</p>
+        <button class="blog-hero__button button button-outline button-outline--white">Читать</button>
+      </div>
+      <!-- /.blog-hero -->
+    </div>
+    <!-- /.container -->
+
+    <div class="blog-hero-image"></div>
+  </section>
+</main>
+
+<?php include_once('partials/footer.php') ?>
