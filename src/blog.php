795031ed004f0f12a6bb8b4c02a8a9ca5a97b06e
Nikita <main.acr0matic@gmail.com>
2021-06-22 03:07:05 +0300

    feat: страница "блог"

--- src/blog.php
+++ src/blog.php
@@ -15,6 +15,246 @@
 
     <div class="blog-hero-image"></div>
   </section>
+
+  <section id="blog">
+    <div class="container">
+      <div class="blog">
+        <h1 class="blog__title page__title section__title">Блог <span>Brand&Digital</span></h1>
+        <div class="blog__filters blog-filters">
+          <div class="blog-filters__title">Фильтры</div>
+
+          <div class="blog-filters__wrapper">
+            <div class="blog-filters__item blog-filters__item--blue">
+              Интерьер
+            </div>
+            <!-- /.blog-filters__item -->
+
+            <div class="blog-filters__item blog-filters__item--green">
+              Дизайн
+            </div>
+            <!-- /.blog-filters__item -->
+
+            <div class="blog-filters__item blog-filters__item--purple">
+              Брендирование
+            </div>
+            <!-- /.blog-filters__item -->
+
+            <div class="blog-filters__item blog-filters__item--gray">
+              Сувениры
+            </div>
+            <!-- /.blog-filters__item -->
+          </div>
+          <!-- /.blog-filters__wrapper -->
+        </div>
+        <!-- /.blog__filters blog-filters -->
+
+        <div class="blog__wrapper">
+          <div class="blog__item blog-item">
+            <div class="row">
+              <div class="col-12 col-lg-5 mb-3 mb-lg0">
+                <picture class="blog-item__picture">
+                  <img src="img/page/blog/1.jpg" alt="" class="blog-item__image">
+                </picture>
+              </div>
+              <!-- /.col-5 -->
+
+              <div class="col-12 col-lg-7">
+                <h3 class="blog-item__title">
+                  Оптимальная температура или как погодные условия могут навредить оклейке автомобиля
+                </h3>
+
+                <p class="blog-item__description">
+                  Брендировать автомобиль пленкой лучше всего при температуре от 60 до 80 градусов по Фаренгейту.
+                </p>
+
+                <div class="blog-filters blog-filters--small">
+                  <div class="blog-filters__wrapper">
+                    <div class="blog-filters__item blog-filters__item--blue">
+                      Интерьер
+                    </div>
+                    <!-- /.blog-filters__item -->
+
+                    <div class="blog-filters__item blog-filters__item--green">
+                      Интерьер
+                    </div>
+                    <!-- /.blog-filters__item -->
+
+                    <div class="blog-filters__item blog-filters__item--purple">
+                      Интерьер
+                    </div>
+                    <!-- /.blog-filters__item -->
+
+                    <div class="blog-filters__item blog-filters__item--gray">
+                      Интерьер
+                    </div>
+                    <!-- /.blog-filters__item -->
+                  </div>
+                  <!-- /.blog-filters__wrapper -->
+                </div>
+                <!-- /.blog__filters blog-filters -->
+              </div>
+              <!-- /.col-7 -->
+            </div>
+            <!-- /.row -->
+          </div>
+          <!-- /.blog__item blog-item -->
+          <div class="blog__item blog-item">
+            <div class="row">
+              <div class="col-12 col-lg-5 mb-3 mb-lg0">
+                <picture class="blog-item__picture">
+                  <img src="img/page/blog/2.jpg" alt="" class="blog-item__image">
+                </picture>
+              </div>
+              <!-- /.col-5 -->
+
+              <div class="col-12 col-lg-7">
+                <h3 class="blog-item__title">
+                  Оптимальная температура или как погодные условия могут навредить оклейке автомобиля
+                </h3>
+
+                <p class="blog-item__description">
+                  Брендировать автомобиль пленкой лучше всего при температуре от 60 до 80 градусов по Фаренгейту.
+                </p>
+
+                <div class="blog-filters blog-filters--small">
+                  <div class="blog-filters__wrapper">
+                    <div class="blog-filters__item blog-filters__item--blue">
+                      Интерьер
+                    </div>
+                    <!-- /.blog-filters__item -->
+
+                    <div class="blog-filters__item blog-filters__item--green">
+                      Интерьер
+                    </div>
+                    <!-- /.blog-filters__item -->
+
+                    <div class="blog-filters__item blog-filters__item--purple">
+                      Интерьер
+                    </div>
+                    <!-- /.blog-filters__item -->
+
+                    <div class="blog-filters__item blog-filters__item--gray">
+                      Интерьер
+                    </div>
+                    <!-- /.blog-filters__item -->
+                  </div>
+                  <!-- /.blog-filters__wrapper -->
+                </div>
+                <!-- /.blog__filters blog-filters -->
+              </div>
+              <!-- /.col-7 -->
+            </div>
+            <!-- /.row -->
+          </div>
+          <!-- /.blog__item blog-item -->
+
+          <div class="blog__item blog-item">
+            <div class="row">
+              <div class="col-12 col-lg-5 mb-3 mb-lg0">
+                <picture class="blog-item__picture">
+                  <img src="img/page/blog/3.jpg" alt="" class="blog-item__image">
+                </picture>
+              </div>
+              <!-- /.col-5 -->
+
+              <div class="col-12 col-lg-7">
+                <h3 class="blog-item__title">
+                  Оптимальная температура или как погодные условия могут навредить оклейке автомобиля
+                </h3>
+
+                <p class="blog-item__description">
+                  Брендировать автомобиль пленкой лучше всего при температуре от 60 до 80 градусов по Фаренгейту.
+                </p>
+
+                <div class="blog-filters blog-filters--small">
+                  <div class="blog-filters__wrapper">
+                    <div class="blog-filters__item blog-filters__item--blue">
+                      Интерьер
+                    </div>
+                    <!-- /.blog-filters__item -->
+
+                    <div class="blog-filters__item blog-filters__item--green">
+                      Интерьер
+                    </div>
+                    <!-- /.blog-filters__item -->
+
+                    <div class="blog-filters__item blog-filters__item--purple">
+                      Интерьер
+                    </div>
+                    <!-- /.blog-filters__item -->
+
+                    <div class="blog-filters__item blog-filters__item--gray">
+                      Интерьер
+                    </div>
+                    <!-- /.blog-filters__item -->
+                  </div>
+                  <!-- /.blog-filters__wrapper -->
+                </div>
+                <!-- /.blog__filters blog-filters -->
+              </div>
+              <!-- /.col-7 -->
+            </div>
+            <!-- /.row -->
+          </div>
+          <!-- /.blog__item blog-item -->
+
+          <div class="blog__item blog-item">
+            <div class="row">
+              <div class="col-12 col-lg-5 mb-3 mb-lg0">
+                <picture class="blog-item__picture">
+                  <img src="img/page/blog/4.jpg" alt="" class="blog-item__image">
+                </picture>
+              </div>
+              <!-- /.col-5 -->
+
+              <div class="col-12 col-lg-7">
+                <h3 class="blog-item__title">
+                  Оптимальная температура или как погодные условия могут навредить оклейке автомобиля
+                </h3>
+
+                <p class="blog-item__description">
+                  Брендировать автомобиль пленкой лучше всего при температуре от 60 до 80 градусов по Фаренгейту.
+                </p>
+
+                <div class="blog-filters blog-filters--small">
+                  <div class="blog-filters__wrapper">
+                    <div class="blog-filters__item blog-filters__item--blue">
+                      Интерьер
+                    </div>
+                    <!-- /.blog-filters__item -->
+
+                    <div class="blog-filters__item blog-filters__item--green">
+                      Интерьер
+                    </div>
+                    <!-- /.blog-filters__item -->
+
+                    <div class="blog-filters__item blog-filters__item--purple">
+                      Интерьер
+                    </div>
+                    <!-- /.blog-filters__item -->
+
+                    <div class="blog-filters__item blog-filters__item--gray">
+                      Интерьер
+                    </div>
+                    <!-- /.blog-filters__item -->
+                  </div>
+                  <!-- /.blog-filters__wrapper -->
+                </div>
+                <!-- /.blog__filters blog-filters -->
+              </div>
+              <!-- /.col-7 -->
+            </div>
+            <!-- /.row -->
+          </div>
+          <!-- /.blog__item blog-item -->
+        </div>
+        <!-- /.blog__wrapper -->
+      </div>
+      <!-- /.blog -->
+    </div>
+    <!-- /.container -->
+  </section>
+  <!-- /#blog.section -->
 </main>
 
 <?php include_once('partials/footer.php') ?>
