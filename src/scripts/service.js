0eba8b4b936668932ddc3d99154f2ef1208caf1f
Nikita <main.acr0matic@gmail.com>
2021-06-23 15:41:30 +0300

    feat: верстка хедера страницы "оклейка авто"

--- src/scripts/service.js
+++ src/scripts/service.js
@@ -7,3 +7,19 @@ if (serviceSection) {
   else if (serviceCards.length === 6) serviceWrapper.classList.add('service__wrapper--six-items')
   else if (serviceCards.length === 4) serviceWrapper.classList.add('service__wrapper--four-items')
 }
+
+const serviceContentSection = document.getElementById('service-content');
+
+if (serviceContentSection) {
+  const serviceItems = serviceContentSection.querySelectorAll('.service-content-tabs__item');
+
+  serviceItems.forEach(item => {
+    item.addEventListener('click', () => {
+      const activeItem = document.querySelector('.service-content-tabs__item--active');
+      if (activeItem) activeItem.classList.remove('service-content-tabs__item--active');
+
+      item.classList.add('service-content-tabs__item--active');
+    });
+  });
+
+}
---
+++ src/service-page.php
@@ -0,0 +1,92 @@
+<?php include_once('partials/header.php') ?>
+
+<!-- Основной контент -->
+<main>
+  <section id="service-content">
+    <div class="service-header">
+      <div class="container">
+        <h1 class="service-header__title page__title section__title">Брендирование авто</h1>
+
+        <div class="service-header__tabs service-content-tabs">
+          <div class="service-content-tabs__item service-content-tabs__item--active">
+            <div class="service-content-tabs__picture">
+              <img src="img/page/service-content/1.png" alt="" class="service-content-tabs__image">
+            </div>
+            <!-- /.service-content-tabs__picture -->
+
+            <div class="service-content-tabs__title">Легковое авто</div>
+          </div>
+          <!-- /.service-content-tabs__item service-content-tabs__item--active -->
+
+          <div class="service-content-tabs__item">
+            <div class="service-content-tabs__picture">
+              <img src="img/page/service-content/2.png" alt="" class="service-content-tabs__image">
+            </div>
+            <!-- /.service-content-tabs__picture -->
+
+            <div class="service-content-tabs__title">Легковое авто</div>
+          </div>
+          <!-- /.service-content-tabs__item -->
+
+          <div class="service-content-tabs__item">
+            <div class="service-content-tabs__picture">
+              <img src="img/page/service-content/3.png" alt="" class="service-content-tabs__image">
+            </div>
+            <!-- /.service-content-tabs__picture -->
+
+            <div class="service-content-tabs__title">Легковое авто</div>
+          </div>
+          <!-- /.service-content-tabs__item -->
+
+          <div class="service-content-tabs__item">
+            <div class="service-content-tabs__picture">
+              <img src="img/page/service-content/4.png" alt="" class="service-content-tabs__image">
+            </div>
+            <!-- /.service-content-tabs__picture -->
+
+            <div class="service-content-tabs__title">Легковое авто</div>
+          </div>
+          <!-- /.service-content-tabs__item -->
+
+          <div class="service-content-tabs__item">
+            <div class="service-content-tabs__picture">
+              <img src="img/page/service-content/5.png" alt="" class="service-content-tabs__image">
+            </div>
+            <!-- /.service-content-tabs__picture -->
+
+            <div class="service-content-tabs__title">Легковое авто</div>
+          </div>
+          <!-- /.service-content-tabs__item -->
+        </div>
+        <!-- /.service-content__tabs service-content-tabs -->
+      </div>
+      <!-- /.container -->
+    </div>
+    <!-- /.service-content__header -->
+
+    <div class="service-content">
+      <div class="container">
+        <div class="swiper-container service-content-slider">
+          <div class="swiper-wrapper">
+            <div class="swiper-slide">
+
+            </div>
+            <!-- /.swiper-slide -->
+          </div>
+          <!-- /.swiper-wrapper -->
+        </div>
+        <!-- /.swiper-container service-content-slider -->
+      </div>
+      <!-- /.container -->
+    </div>
+    <!-- /.service-content -->
+  </section>
+
+  <?php include_once('partials/confidence.php') ?>
+
+  <?php include_once('partials/portfolio.php') ?>
+
+  <?php include_once('partials/callback.php') ?>
+</main>
+
+<?php include_once('partials/footer.php') ?>
