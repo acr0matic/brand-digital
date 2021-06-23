b2564128d2e1f67a3432011f879a9ce98c1de3b2
Nikita <main.acr0matic@gmail.com>
2021-06-23 14:48:50 +0300

    feat: верстка страницы "поста"

---
+++ src/blog-post.php
@@ -0,0 +1,99 @@
+<?php include_once('partials/header.php') ?>
+
+<!-- Основной контент -->
+<main>
+  <section id="blog-post">
+    <img src="img/page/blog/banner.jpg" alt="" class="blog-post-banner cms-image">
+
+    <div class="container">
+      <div class="blog-post">
+        <div class="blog-post__filters blog-filters blog-filters--small">
+          <div class="blog-filters__wrapper">
+            <div class="blog-filters__item blog-filters__item--blue">
+              Интерьер
+            </div>
+            <!-- /.blog-filters__item -->
+
+            <div class="blog-filters__item blog-filters__item--green">
+              Дизайн
+            </div>
+            <!-- /.blog-filters__item -->
+
+            <div class="blog-filters__item blog-filters__item--purple">
+              Брендирование
+            </div>
+            <!-- /.blog-filters__item -->
+
+            <div class="blog-filters__item blog-filters__item--gray">
+              Сувениры
+            </div>
+            <!-- /.blog-filters__item -->
+          </div>
+          <!-- /.blog-filters__wrapper -->
+        </div>
+        <!-- /.blog__filters blog-filters -->
+
+        <div class="blog-post__content">
+          <h1 class="blog-post__title page__title section__title">Оптимальная температура или как погодные условия могут навредить оклейке автомобиля</h1>
+          <h2 class="blog-post__subtitle section__title">Брендировать автомобиль пленкой <br> лучше всего при температуре от 60 до 80 <br> градусов по Фаренгейту</h2>
+
+          <picture class="blog-post__picture">
+            <img src="img/page/blog/photo.jpg" alt="" class="blog-post__image cms-image">
+          </picture>
+
+          <div class="row">
+            <div class="col-12 col-lg-7 pe-lg-5">
+              <div class="blog-post__text">
+                <p>Брендировать автомобиль пленкой лучше всего при температуре от 60 до 80 градусов по Фаренгейту. Согласно данным Avery и 3M – ведущих производителей автомобильных пленок, такая температура идеальна для успешного долгосрочного брендирования авто. Виниловые пленки очень быстро адаптируются к температуре рабочего пространства. Однако холодный день в январе или жаркий день в августе не должны помешать вам оклеить пленкой ваши новые транспортные средства. У профессиональной компании, занимающейся брендированием авто, будет чистое, сухое, контролируемое по температуре оборудование для создания идеальных условий, необходимых для оклейки высокого качества.
+                </p>
+                <br>
+                <p>Брендировать автомобиль пленкой лучше всего при температуре от 60 до 80 градусов по Фаренгейту. Согласно данным Avery и 3M – ведущих производителей автомобильных пленок, такая температура идеальна для успешного долгосрочного брендирования авто. Виниловые пленки очень быстро адаптируются к температуре рабочего пространства. Однако холодный день в январе или жаркий день в августе не должны помешать вам оклеить пленкой ваши новые транспортные средства. У профессиональной компании, занимающейся брендированием авто, будет чистое, сухое, контролируемое по температуре оборудование для создания идеальных условий, необходимых для оклейки высокого качества.
+                </p>
+                <br>
+                <p>Это просто оклейка автомобиля. Что может пойти не так?
+                  <br> <br>
+
+                  <b>Слишком холодно</b> <br>
+                  Если транспортное средство, пленка и рабочее пространство слишком холодные, то «обертка» становится жесткой и ее трудно приклеить на сложные формы и изгибы автомобиля. Это не только сделает оклейку транспортного средства более трудоемким процессом для мастера, но также повлияет на то, насколько хорошо пленка приклеится к поверхности авто. Более низкие температуры ограничивают хорошие адгезионные свойства, что увеличивает риск разрушения пленки из-за низкого уровня сцепления с поверхностью.
+                </p>
+              </div>
+            </div>
+            <!-- /.col-7 -->
+
+            <div class="col-5">
+              <div class="blog-post__extra">
+                <picture class="blog-post__picture blog-post__picture--small">
+                  <img src="img/page/blog/photo__2.jpg" alt="" class="blog-post__image cms-image">
+                </picture>
+
+                <h3 class="blog-post__info">Брендировать автомобиль пленкой лучше всего при температуре от 60 до 80 градусов по Фаренгейту. </h3>
+              </div>
+            </div>
+            <!-- /.col-5 -->
+          </div>
+          <!-- /.row -->
+        </div>
+        <!-- /.blog-post__content -->
+      </div>
+      <!-- /.blog-post -->
+    </div>
+
+    <div class="blog-post-nav">
+
+      <div class="blog-post-nav__action blog-post-nav__action--left">
+        Немного статистики на
+        американских реалиях
+      </div>
+      <!-- /.blog-post-nav__action blog-post-nav__action--left -->
+
+      <div class="blog-post-nav__action blog-post-nav__action--right">
+        Немного статистики на
+        американских реалиях
+      </div>
+      <!-- /.blog-post-nav__action blog-post-nav__action--left -->
+    </div>
+    <!-- /.blog-post-nav -->
+  </section>
+</main>
+
+<?php include_once('partials/footer.php') ?>
