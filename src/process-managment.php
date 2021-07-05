c3b755ea7dfc5f5d48cac61aff2312af4cdd394a
Nikita <main.acr0matic@gmail.com>
2021-07-05 12:30:00 +0300

    feat: страница "Процесс управления"

---
+++ src/process-managment.php
@@ -0,0 +1,137 @@
+<?php include_once('partials/header.php') ?>
+
+<!-- Основной контент -->
+<main>
+  <section id="process" class="process-managment">
+    <div class="container">
+      <div class="process__heading process-heading process-heading--underline">
+        <div class="row">
+          <div class="col-12 col-lg-6">
+            <h2 class="process-heading__title section__title">Управление проектами</h2>
+          </div>
+          <!-- /.col-12 col-lg-6 -->
+
+          <div class="col-12 col-lg-6">
+            <p class="process-heading__text section__text">Мы всегда будем активно управлять вашим проектом. Будем под рукой, чтобы помочь вам на каждом шагу и быстро решить проблемы, когда они возникают. Все, что мы делаем, ориентировано на вас и ваши потребности.</p>
+          </div>
+          <!-- /.col-12 col-lg-6 -->
+        </div>
+        <!-- /.row -->
+      </div>
+      <!-- /.process__heading -->
+
+      <div class="process__content process-content align-items-center">
+        <div class="process-content__wrapper">
+          <div class="row">
+            <div class="col-12 col-lg-6">
+              <div class="process-content__textblock process-textblock">
+                <div class="process-textblock__step">
+                  1
+                </div>
+                <!-- /.process-textblock__step -->
+
+                <div class="process-textblock__wrapper">
+                  <h3 class="process-textblock__title">Свяжитесь с нами</h3>
+                  <p class="process-textblock__text">Вы можете сделать это любым удобным способом и поделиться своей идеей. Есть возможность оставить заявку для обратной связи на сайте, разработать свой макет в нашем конструкторе или просто позвонить/написать нашему менеджеру.</p>
+                </div>
+                <!-- /.process-textblock__wrapper -->
+              </div>
+              <!-- /.process-content__item process-textblock -->
+            </div>
+            <!-- /.col-12 col-lg-6 -->
+
+            <div class="col-12 col-lg-6">
+              <img src="img/page/process/managment/1.svg" alt="" class="process-textblock__image">
+            </div>
+            <!-- /.col-12 col-lg-6 -->
+          </div>
+          <!-- /.row -->
+
+          <div class="row flex-lg-row-reverse align-items-center">
+            <div class="col-12 col-lg-6">
+              <div class="process-content__textblock process-textblock">
+                <div class="process-textblock__step">
+                  2
+                </div>
+                <!-- /.process-textblock__step -->
+
+                <div class="process-textblock__wrapper">
+                  <h3 class="process-textblock__title">Персональный менеджер</h3>
+                  <p class="process-textblock__text">Специалист будет закреплен за Вашей заявкой. Совместно с менеджером Вы составите техническое задание для реализации Вашей задачи с наилучшими параметрами качества и цены. Мы всегда с Вами на связи и информируем о всех этапах исполнения проекта.</p>
+                </div>
+                <!-- /.process-textblock__wrapper -->
+              </div>
+              <!-- /.process-content__item process-textblock -->
+            </div>
+            <!-- /.col-12 col-lg-6 -->
+
+            <div class="col-12 col-lg-6">
+              <img src="img/page/process/managment/2.svg" alt="" class="process-textblock__image">
+            </div>
+            <!-- /.col-12 col-lg-6 -->
+          </div>
+          <!-- /.row -->
+
+          <div class="row align-items-center">
+            <div class="col-12 col-lg-6">
+              <div class="process-content__textblock process-textblock">
+                <div class="process-textblock__step">
+                  3
+                </div>
+                <!-- /.process-textblock__step -->
+
+                <div class="process-textblock__wrapper">
+                  <h3 class="process-textblock__title">Заключение договора <br> и запуск проекта</h3>
+                  <p class="process-textblock__text">Следующим шагом идёт согласование суммы заказа и составление договора. После всех согласований мы переходим к последнему этапу.</p>
+                </div>
+                <!-- /.process-textblock__wrapper -->
+              </div>
+              <!-- /.process-content__item process-textblock -->
+            </div>
+            <!-- /.col-12 col-lg-6 -->
+
+            <div class="col-12 col-lg-6">
+              <img src="img/page/process/managment/3.svg" alt="" class="process-textblock__image">
+            </div>
+            <!-- /.col-12 col-lg-6 -->
+          </div>
+          <!-- /.row -->
+
+          <div class="row flex-lg-row-reverse align-items-center">
+            <div class="col-12 col-lg-6">
+              <div class="process-content__textblock process-textblock">
+                <div class="process-textblock__step">
+                  4
+                </div>
+                <!-- /.process-textblock__step -->
+
+                <div class="process-textblock__wrapper">
+                  <h3 class="process-textblock__title">Внесение предоплаты</h3>
+                  <p class="process-textblock__text">Вы вносите предоплату и мы приступаем к брендированию.</p>
+                </div>
+                <!-- /.process-textblock__wrapper -->
+              </div>
+              <!-- /.process-content__item process-textblock -->
+            </div>
+            <!-- /.col-12 col-lg-6 -->
+
+            <div class="col-12 col-lg-6">
+              <img src="img/page/process/managment/4.svg" alt="" class="process-textblock__image">
+            </div>
+            <!-- /.col-12 col-lg-6 -->
+          </div>
+          <!-- /.row -->
+        </div>
+        <!-- /.process-content__wrapper -->
+
+        <button class="process-content__button button button-primary">Начнём проект</button>
+      </div>
+      <!-- /.process__content -->
+    </div>
+    <!-- /.container -->
+  </section>
+
+  <?php include_once('partials/callback.php') ?>
+</main>
+
+<?php include_once('partials/footer.php') ?>
