752107df5533b4ecf687b1e3f48f43c0f49c0ec2
Nikita <main.acr0matic@gmail.com>
2021-07-06 02:34:44 +0300

    feat: страница "процессы - монтаж"

--- src/process-assembly.php
+++ src/process-assembly.php
@@ -2,17 +2,17 @@
 
 <!-- Основной контент -->
 <main>
-  <section id="process" class="process process-manufacturing">
+  <section id="process" class="process process-assembly">
     <div class="container">
       <div class="process__heading process-heading">
         <div class="row">
           <div class="col-12 col-lg-6">
-            <h2 class="process-heading__title section__title">Производство</h2>
+            <h2 class="process-heading__title section__title">Монтаж плёнки</h2>
           </div>
           <!-- /.col-12 col-lg-6 -->
 
           <div class="col-12 col-lg-6">
-            <p class="process-heading__text section__text">Контроль качества на каждом этапе производства является для нас первостепенной задачей по выполнению заказа. Мы инвестируем в современное, передовое оборудование</p>
+            <p class="process-heading__text section__text">После завершения изготовления Вашего заказа персональный менеджер свяжется с Вами и проинформирует о готовности к монтажу</p>
           </div>
           <!-- /.col-12 col-lg-6 -->
         </div>
@@ -23,20 +23,20 @@
     <!-- /.container -->
 
     <div class="process__banner">
-      <img class="cms-image" src="img/page/process/manufacturing/banner.jpg" alt="">
+      <img class="cms-image" src="img/page/process/assembly/banner.jpg" alt="">
     </div>
     <!-- /.process__banner -->
 
     <div class="container">
       <div class="process__content process-content align-items-center">
-        <h2 class="process-content__title">Производственные мощности</h2>
+        <h2 class="process-content__title">Используем только проверенные инструменты</h2>
         <div class="process-content__wrapper">
           <div class="row flex-column-reverse flex-lg-row align-items-center">
             <div class="col-12 col-lg-5 col-xxl-6">
               <div class="process-content__textblock process-textblock">
                 <div class="process-textblock__wrapper">
-                  <h3 class="process-textblock__title">RF-640, серии VersaEXPRESS</h3>
-                  <p class="process-textblock__text">Плоттер Roland RF-640 серии VersaEXPRESS обладает впечатляющей скоростью печати и качеством, повышенной производительностью и непревзойденной надежностью.</p>
+                  <h3 class="process-textblock__title">Нож OLFA svr-2</h3>
+                  <p class="process-textblock__text">Профессиональный стандартный нож, полностью изготовленный из нержавеющей стали. Нержавеющее стальное лезвие мягче, чем лезвие из углеродистой стали, и не оставляет царапин и рисок при тонировании обычного стекла, сталинита и триплекса</p>
                 </div>
                 <!-- /.process-textblock__wrapper -->
               </div>
@@ -45,7 +45,7 @@
             <!-- /.col-12 col-lg-5 col-xxl-6 -->
 
             <div class="col-12 col-lg-7 col-xxl-6 mb-3 mb-lg-0">
-              <img src="img/page/process/manufacturing/1.jpg" alt="" class="process-textblock__image">
+              <img src="img/page/process/assembly/1.jpg" alt="" class="process-textblock__image">
             </div>
             <!-- /.col-12 col-lg-5 col-xxl-6 -->
           </div>
@@ -55,8 +55,8 @@
             <div class="col-12 col-lg-5 col-xxl-6 ps-lg-5">
               <div class="process-content__textblock process-textblock">
                 <div class="process-textblock__wrapper">
-                  <h3 class="process-textblock__title">Camm-1 GR-640</h3>
-                  <p class="process-textblock__text">Профессиональный режущий плоттер Roland GR-640. Теперь можно резать то, что раньше вам казалось непрорезаемым на обычном режущем плоттере.</p>
+                  <h3 class="process-textblock__title">Ракель Avery</h3>
+                  <p class="process-textblock__text">Инструмент предназначен для разглаживания и нанесения виниловых пленок. Боковая плоскость снабжена небольшим отрезком фетра шириной 17 мм. Он мягче и эластичней предыдущих моделей и прекрасно подойдёт для нанесения самоклеющейся пленки на изогнутые и гофрированные поверхности.</p>
                 </div>
                 <!-- /.process-textblock__wrapper -->
               </div>
@@ -65,7 +65,7 @@
             <!-- /.col-12 col-lg-5 col-xxl-6 -->
 
             <div class="col-12 col-lg-7 col-xxl-6 mb-3 mb-lg-0">
-              <img src="img/page/process/manufacturing/2.jpg" alt="" class="process-textblock__image">
+              <img src="img/page/process/assembly/2.jpg" alt="" class="process-textblock__image">
             </div>
             <!-- /.col-12 col-lg-7 col-xxl-6 -->
           </div>
@@ -75,8 +75,8 @@
             <div class="col-12 col-lg-5 col-xxl-6">
               <div class="process-content__textblock process-textblock">
                 <div class="process-textblock__wrapper">
-                  <h3 class="process-textblock__title">Ламинатор Mefu MF1700-С3 Trimmer</h3>
-                  <p class="process-textblock__text">Ламинатор Mefu MF1700-C3 для одностороннего горячего и холодного ламинирования с интегрированной функцией продольной резки ламинированного изделия.</p>
+                  <h3 class="process-textblock__title">Магнитный держатель для пленки</h3>
+                  <p class="process-textblock__text">Магнитные держатели применяются для фиксации пленки при оклеивании. Держатель имеет удобную ручку, магнит находится в прорезиненном покрытии, что позволяет не беспокоиться о повреждении ЛКП при работе с магнитами-фиксаторами. Для работы с большими деталями кузова автомобиля понадобится не менее двух магнитов для пленки.</p>
                 </div>
                 <!-- /.process-textblock__wrapper -->
               </div>
@@ -85,7 +85,7 @@
             <!-- /.col-12 col-lg-5 col-xxl-6 -->
 
             <div class="col-12 col-lg-7 col-xxl-6 mb-3 mb-lg-0">
-              <img src="img/page/process/manufacturing/3.jpg" alt="" class="process-textblock__image">
+              <img src="img/page/process/assembly/3.jpg" alt="" class="process-textblock__image">
             </div>
             <!-- /.col-12 col-lg-7 col-xxl-6 -->
           </div>
@@ -99,11 +99,9 @@
           <div class="row">
             <div class="col-12 col-lg-5">
               <ol class="process-content__list list">
-                <li class="list__item list__item--active">Печать</li>
-                <li class="list__item">Ламинация</li>
-                <li class="list__item">Резка</li>
-                <li class="list__item">Постпечатная подготовка</li>
-                <li class="list__item">Упаковка и доставка</li>
+                <li class="list__item list__item--active">Подготовка помещения</li>
+                <li class="list__item">Подготовка поверхности авто</li>
+                <li class="list__item">Монтаж</li>
               </ol>
             </div>
             <!-- /.col-5 -->
@@ -113,35 +111,21 @@
                 <div class="swiper-wrapper">
                   <div class="swiper-slide">
                     <picture>
-                      <img src="img/page/process/manufacturing/slider-small/1.jpg" class="cms-image" alt="">
+                      <img src="img/page/process/assembly/slider-small/1.jpg" class="cms-image" alt="">
                     </picture>
                   </div>
                   <!-- /.swiper-slide -->
 
                   <div class="swiper-slide">
                     <picture>
-                      <img src="img/page/process/manufacturing/slider-small/2.jpg" class="cms-image" alt="">
+                      <img src="img/page/process/assembly/slider-small/2.jpg" class="cms-image" alt="">
                     </picture>
                   </div>
                   <!-- /.swiper-slide -->
 
                   <div class="swiper-slide">
                     <picture>
-                      <img src="img/page/process/manufacturing/slider-small/3.jpg" class="cms-image" alt="">
-                    </picture>
-                  </div>
-                  <!-- /.swiper-slide -->
-
-                  <div class="swiper-slide">
-                    <picture>
-                      <img src="img/page/process/manufacturing/slider-small/4.jpg" class="cms-image" alt="">
-                    </picture>
-                  </div>
-                  <!-- /.swiper-slide -->
-
-                  <div class="swiper-slide">
-                    <picture>
-                      <img src="img/page/process/manufacturing/slider-small/5.jpg" class="cms-image" alt="">
+                      <img src="img/page/process/assembly/slider-small/3.jpg" class="cms-image" alt="">
                     </picture>
                   </div>
                   <!-- /.swiper-slide -->
