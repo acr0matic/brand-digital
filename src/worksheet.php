270ecb1b37515709e1bd516ef4f0e2974e3c0314
Nikita <main.acr0matic@gmail.com>
2022-03-25 16:17:07 +0300

    feat: база для новой страницы

---
+++ src/worksheet.php
@@ -0,0 +1,9 @@
+<?php include_once('partials/header.php') ?>
+
+<!-- Основной контент -->
+<main id="worksheet">
+
+  <?php include_once('partials/callback.php') ?>
+</main>
+
+<?php include_once('partials/footer.php') ?>
