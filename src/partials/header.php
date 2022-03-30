6db9ae302952787f35b8909f908a5431b220703c
Nikita <main.acr0matic@gmail.com>
2022-03-30 15:50:48 +0300

    feat: анимация иконки "скачать" и доработка заголовков

--- src/partials/header.php
+++ src/partials/header.php
@@ -129,14 +129,15 @@
                 <div class="dropdown__content">
                   <div class="dropdown">
                     <a href="service.php" class="dropdown__item dropdown__item--arrow">
-                      Оклейка транспорта
+                      Брендирование авто
                       <svg class="dropdown__icon dropdown__icon--right" viewBox="0 0 19 12" fill="none" xmlns="http://www.w3.org/2000/svg">
                         <path d="M1.07617 1.64258L8.98926 9.55566L17.2393 1.64258" stroke="#FF8025" stroke-width="2.5" />
                       </svg>
                     </a>
 
                     <div class="dropdown__content dropdown__content--right">
-                      <a class="dropdown__item dropdown__item--main" href="service.php">Брендирование авто</a>
+                      <a class="dropdown__item dropdown__item--main" href="service.php">Оклейка транспорта</a>
+                      <a class="dropdown__item dropdown__item--main" href="service.php">Реклама на тентах</a>
                       <a class="dropdown__item" href="service-page.php#category-4">Грузовой авто</a>
                       <a class="dropdown__item" href="service-page.php#category-3">Фургон</a>
                       <a class="dropdown__item" href="service-page.php#category-2">Малый грузовой</a>
--- src/partials/seo-block.php
+++ src/partials/seo-block.php
@@ -7,11 +7,10 @@
             <img src="img/section/seo-block/image.jpg" alt="" class="seo-block__image">
             <a href="" class="seo-block__link" download>
               <span>Скачать полное портфолио PDF</span>
-              <svg class="seo-block__icon" width="38" height="33" viewBox="0 0 38 33" fill="none" xmlns="http://www.w3.org/2000/svg">
-                <rect x="18" y="7" width="3" height="25" rx="1.5" fill="#888888" />
-                <rect width="2.72638" height="12.2601" rx="1.36319" transform="matrix(0.752808 0.65824 -0.749352 0.662172 26.8086 23.0872)" fill="#888888" />
-                <rect width="2.72638" height="12.2601" rx="1.36319" transform="matrix(-0.752808 0.65824 0.749352 0.662172 12.3223 23.0872)" fill="#888888" />
-                <path d="M2 8.42788V2H36V8.42788" stroke="#888888" stroke-width="3" stroke-linecap="round" />
+
+              <svg class="seo-block__icon" width="100%" height="100%" viewBox="0 0 36 36" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path class="arrow seo-block__arrow" fill-rule="evenodd" clip-rule="evenodd" d="M17.2651 8.04871C17.2651 7.27266 17.8942 6.64355 18.6703 6.64355C19.4463 6.64355 20.0754 7.27266 20.0754 8.04871V26.5224L24.5602 22.5595C25.0887 22.0925 25.9475 22.0902 26.4784 22.5544C27.0093 23.0187 27.0113 23.7736 26.4828 24.2406L19.7904 30.1544C19.501 30.4102 19.1124 30.5265 18.7329 30.5032C18.3534 30.5265 17.9648 30.4102 17.6754 30.1544L10.983 24.2406C10.4545 23.7736 10.4565 23.0187 10.9874 22.5544C11.5183 22.0902 12.3771 22.0925 12.9056 22.5595L17.2651 26.4118V8.04871Z" fill="#888888" />
+                <path class="rect" d="M2.2793 7.98141V1.95996H34.1295V7.98141" stroke="#888888" stroke-width="3" stroke-linecap="round" />
               </svg>
             </a>
 
